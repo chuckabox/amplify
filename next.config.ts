bca169663445482897b6026eb30a574ea0107048
chuckabox <chuckaboxpeter@gmail.com>
2026-07-25 13:49:34 +1000

    Set basePath to /amplify

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export",
+  // Deployed to https://chuckabox.github.io/amplify/
+  basePath: "/amplify",
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
